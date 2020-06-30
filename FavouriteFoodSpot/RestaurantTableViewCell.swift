e193ed8f1eb65624c0ada4f51cc17bafa67349c5
Venkata Bhargav <bhargav.venkata.a@gmail.com>
2020-06-30 15:41:07 +0530

    Create an app that displays list of restaurants with their name, location & type by using the UITableVIewController  and by using custom table view cell instead of basic cell type.

---
+++ FavouriteFoodSpot/RestaurantTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  RestaurantTableViewCell.swift
+//  FavouriteFoodSpot
+//
+//  Created by Venkata Bhargav on 30/06/20.
+//  Copyright © 2020 venkat bhargav. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
